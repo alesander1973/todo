ab8b573e847cc3192e90e776f7d9e74d891e32c3
sasha <zaalmix@gmail.com>
2019-06-09 17:18:11 +0300

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AutofocusModule } from 'angular-autofocus-fix';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AutofocusModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
